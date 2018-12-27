abe0312e205b347f0308287498fdffae21310d78
Andrzej Stella-Sawicki <astellasawicki@cloudferro.com>
2018-12-27 14:43:44 +0100

    Updated to older python3

--- timer.py
+++ timer.py
@@ -4,28 +4,28 @@ import copy
 
 class Timer:
 
-    count: int = 0
+    count = 0
 
     def __init__(self):
         Timer.count += 1
         self.start_time = time.time()
         # Default values for end_time and duration in case the stopper has not been stopped yet
-        self.end_time: time.time = -1
-        self.duration: time.time = -1
+        self.end_time = -1
+        self.duration = -1
 
-    def stop(self) -> time.time:
+    def stop(self):
         self.end_time = time.time()
         self.duration = self.end_time - self.start_time
         return self.end_time
 
-    def lap(self) -> time.time:
+    def lap(self):
         return time.time() - self.start_time
 
     def restart(self):
         self.start_time = time.time()
         return self.start_time
 
-    def fork(self) -> object:
+    def fork(self):
         """
 
         :rtype: object
